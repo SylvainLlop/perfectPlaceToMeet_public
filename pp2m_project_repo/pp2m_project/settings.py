c89fda11b8391c2294ca311f4b2a45a88af7b680
Sylvain Llop <sylvain.llop@gmail.com>
2020-12-01 14:54:43 +0100

    django_heroku added to settings

--- pp2m_project_repo/pp2m_project/settings.py
+++ pp2m_project_repo/pp2m_project/settings.py
@@ -11,13 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 import dj_database_url
 
-# ...
-if os.environ.get('ENV') == 'PRODUCTION':
-    # ...
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -153,3 +149,6 @@ if DEBUG == False:
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
