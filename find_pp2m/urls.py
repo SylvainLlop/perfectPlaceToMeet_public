bf660ec7799e54abc62928c59a3f80800e57c54f
Sylvain Llop <sylvain.llop@gmail.com>
2020-12-01 16:20:31 +0100

    url path changed

--- find_pp2m/urls.py
+++ find_pp2m/urls.py
@@ -5,8 +5,7 @@ from . import views
 
 app_name = 'pp2m'
 urlpatterns = [
-    path('', views.index, name='index'),
     # path('departements/', views.listing_dept, name="listing_dept"),
     path('pp2m_search/<str:cities_name>', views.pp2m_search, name="pp2m_distance"),
-    path('pp2m/', views.pp2m_form, name="pp2m_form"),
+    path('', views.pp2m_form, name="pp2m_form"),
 ]
